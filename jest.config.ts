d7136e412ef451af9a5ec1e2b1a9ba1492b9ba2d
Sanju Ghandhi <ghandhisanju@gmail.com>
2024-06-19 10:49:30 +0100

    yeah

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,12 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    testMatch: ['**/tests/**/*.test.ts'], // Ensure this matches your test file location
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testMatch: ['**/tests/**/*.test.ts'], // Ensure this matches your test file location
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+};
